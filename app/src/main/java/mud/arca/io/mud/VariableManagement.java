10abc9daaa5dfec9a6f83a0952508b096d9e87c1
asteffen <3031930+asteffen@users.noreply.github.com>
2019-10-30 09:30:07 -0700

    add settings.xml and var management activity

---
+++ app/src/main/java/mud/arca/io/mud/VariableManagement.java
@@ -0,0 +1,13 @@
+package mud.arca.io.mud;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VariableManagement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_variable_management);
+    }
+}
