60d8b80efa952593b1ba22ef4bc83bb815c7ad96
Andrew Steffen <3031930+asteffen@users.noreply.github.com>
2020-02-03 09:44:29 -0800

    Merge branch 'master' of C:\Users\andre\AndroidStudioProjects\mud with conflicts.

--- app/src/main/java/mud/arca/io/mud/ChartTestActivity.java
+++ app/src/main/java/mud/arca/io/mud/ChartTestActivity.java
@@ -1,10 +1,25 @@
 package mud.arca.io.mud;
 
 import androidx.appcompat.app.AppCompatActivity;
-import mud.arca.io.mud.DataStructures.MockUser;
-import mud.arca.io.mud.Views.YearSummaryView;
 
 import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import mud.arca.io.mud.DataStructures.DemoUser;
 
 public class ChartTestActivity extends AppCompatActivity {
 
@@ -13,24 +28,31 @@ public class ChartTestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chart_test);
 
-        // in this example, a LineChart is initialized from xml
-        YearSummaryView chart = findViewById(R.id.chart);
-        chart.setData(new MockUser().getDayData());
+        // in this example, a BarChart is initialized from xml
+        BarChart barChart = (BarChart) findViewById(R.id.chart);
 
-//        List<Entry> entries = new ArrayList<Entry>();
-//
-//        // turn your data into Entry objects
-//        entries.add(new Entry(1, 2));
-//        entries.add(new Entry(2, 4));
-//        entries.add(new Entry(3, 9));
-//
-//        LineDataSet dataSet = new LineDataSet(entries, "Label"); // add entries to dataset
-//        dataSet.setColors(ColorTemplate.VORDIPLOM_COLORS);
-//        // dataSet.setValueTextColor(...); // styling, ...
-//
-//        LineData lineData = new LineData(dataSet);
-//        chart.setData(lineData);
-//        chart.invalidate(); // refresh
+        List<BarEntry> entries = new ArrayList<BarEntry>();
+        entries.add(new BarEntry(0, 4));
+        entries.add(new BarEntry(1, 8));
+        entries.add(new BarEntry(2, 7));
 
+        // add entries to dataset
+        BarDataSet dataSet = new BarDataSet(entries, "Label");
+        dataSet.setColors(ColorTemplate.VORDIPLOM_COLORS);
+        // dataSet.setValueTextColor(...); // styling, ...
+
+        BarData lineData = new BarData(dataSet);
+        barChart.setData(lineData);
+        barChart.invalidate();
+
+        Legend legend = barChart.getLegend();
+        legend.setEnabled(false);
+        Description description = barChart.getDescription();
+        description.setEnabled(false);
+        // Disable the text above each bar for each data pt
+        barChart.setMaxVisibleValueCount(0);
+
+        // test some stuff
+        DemoUser.init();
     }
 }
--- app/src/main/java/mud/arca/io/mud/DataStructures/DemoUser.java
+++ app/src/main/java/mud/arca/io/mud/DataStructures/DemoUser.java
@@ -1,6 +1,25 @@
 package mud.arca.io.mud.DataStructures;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import static mud.arca.io.mud.DataStructures.VarType.FLOAT;
 
 // This class contains data to show demo graphs.
 public class DemoUser {
+
+    User demoUser = new User("DemoUser");
+
+    Variable varSleep = new Variable("Sleep", FLOAT);
+
+    public static void init() {
+        String dt = "2008-01-01";  // Start date
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar c = Calendar.getInstance();
+        //c.setTime(sdf.parse(dt));
+        //c.add(Calendar.DATE, 1);  // number of days to add
+        //dt = sdf.format(c.getTime());  // dt is now the new date
+        System.out.println("qqqq2222");
+    }
+
 }
