f51288cc3097be2bd136462b2959245605db1809
Robert <rr.valdez0312@gmail.com>
2020-02-10 11:54:18 -0800

    created some empty methods to work with

--- app/src/main/java/mud/arca/io/mud/DataStructures/Variable.java
+++ app/src/main/java/mud/arca/io/mud/DataStructures/Variable.java
@@ -2,12 +2,13 @@ package mud.arca.io.mud.DataStructures;
 
 enum VarType
 {
-    INT, FLOAT, BLUE;
+    INT, FLOAT, BOOL;
 }
 
 public class Variable {
     private String name;
     private VarType vartype;
+    private float vardata;
 
     public Variable(String name, VarType vartype) {
         this.name = name;
@@ -29,4 +30,20 @@ public class Variable {
     public void setVartype(VarType vartype) {
         this.vartype = vartype;
     }
+
+    public void setVardata(float vardata) {this.vardata = vardata;}
+
+    /**
+     * setVardata override to accept boolean value
+     * @param vardata boolean value to translate into workable data
+     */
+    public void setVardata(boolean vardata) {
+
+        // verify that boolean matches with VarType enum
+
+        // convert bool into assigned float number
+
+    }
+
+
 }
--- app/src/main/java/mud/arca/io/mud/Settings/VariableManagement.java
+++ app/src/main/java/mud/arca/io/mud/Settings/VariableManagement.java
@@ -13,4 +13,17 @@ public class VariableManagement extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_variable_management);
     }
+
+    public void createVariable() {
+
+        // 1. Get user input
+
+
+        // 1a. Validate user input
+
+        // 2. Create variable object based on input
+
+        // 3. Add to management table
+
+    }
 }
