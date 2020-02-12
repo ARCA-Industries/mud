66318529cf0fec2260b42f1b9ad40a345eca750f
Robert Valdez <rr.valdez0312@gmail.com>
2020-02-12 10:20:21 -0800

    Created Variable Management Screen/User Array for Variables

--- app/src/main/java/mud/arca/io/mud/Settings/VariableManagement.java
+++ app/src/main/java/mud/arca/io/mud/Settings/VariableManagement.java
@@ -1,23 +1,29 @@
 package mud.arca.io.mud.Settings;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import mud.arca.io.mud.DataStructures.Variable;
 import mud.arca.io.mud.R;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 
 public class VariableManagement extends AppCompatActivity {
+
+    public static ArrayList<Variable> variables_test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_variable_management);
+
     }
 
     public void createVariable() {
 
         // 1. Get user input
-
 
         // 1a. Validate user input
 
