a207f567283823ff47631ada10b9ce4fe9f0b94d
Adrian Campos <adriancampos@users.noreply.github.com>
2020-02-12 11:27:47 -0800

    Added log out button in settings

--- app/src/main/java/mud/arca/io/mud/Settings/SettingsFragment.java
+++ app/src/main/java/mud/arca/io/mud/Settings/SettingsFragment.java
@@ -1,13 +1,43 @@
 package mud.arca.io.mud.Settings;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+
 import androidx.preference.PreferenceFragmentCompat;
+import mud.arca.io.mud.LoginScreenActivity;
+import mud.arca.io.mud.MainActivity;
 import mud.arca.io.mud.R;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.settings, rootKey);
+
+        initPreferences();
+    }
+
+
+    private void initPreferences() {
+        // Init auth logout preference
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            findPreference("auth_logout").setSummary(
+                    getString(R.string.preference_auth_logout_summary,
+                            FirebaseAuth.getInstance().getCurrentUser().getUid())
+            );
+        } else {
+            findPreference("auth_logout").setSummary(getString(R.string.preference_auth_logout_summary_error));
+        }
+        findPreference("auth_logout").setOnPreferenceClickListener(preference -> {
+            AuthUI.getInstance().signOut(getContext()).addOnCompleteListener(runnable -> {
+                startActivity(new Intent(getContext(), LoginScreenActivity.class));
+                getActivity().finish();
+            });
+            return true;
+        });
+
+
     }
 }
