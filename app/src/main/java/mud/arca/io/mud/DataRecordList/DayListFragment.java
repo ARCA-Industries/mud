68dccf1b400e09183434f1f4b4b0c2326b8426cd
Adrian Campos <adriancampos@users.noreply.github.com>
2020-02-19 11:57:57 -0800

    Cleaned up DayListFragment

--- app/src/main/java/mud/arca/io/mud/DataRecordList/DayListFragment.java
+++ app/src/main/java/mud/arca/io/mud/DataRecordList/DayListFragment.java
@@ -1,6 +1,5 @@
 package mud.arca.io.mud.DataRecordList;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -28,21 +27,12 @@ import mud.arca.io.mud.DataStructures.User;
 import mud.arca.io.mud.R;
 
 /**
- * A fragment representing a list of Items.
- * <p/>
- * Activities containing this fragment MUST implement the {@link OnListFragmentInteractionListener}
- * interface.
+ * A fragment containing the Day list (Dashboard).
  */
 public class DayListFragment extends Fragment {
 
     // This variable keeps track of which day the user has clicked on. // TODO: Don't do this.
     public static Day daySelected;
-
-    // TODO: Customize parameter argument names
-    private static final String ARG_COLUMN_COUNT = "column-count";
-    // TODO: Customize parameters
-    private int mColumnCount = 1;
-    private OnListFragmentInteractionListener mListener;
 
     private CollectionReference mItemsCollection;
     private FirestoreRecyclerAdapter adapter;
@@ -54,23 +44,9 @@ public class DayListFragment extends Fragment {
     public DayListFragment() {
     }
 
-    // TODO: Customize parameter initialization
-    @SuppressWarnings("unused")
-    public static DayListFragment newInstance(int columnCount) {
-        DayListFragment fragment = new DayListFragment();
-        Bundle args = new Bundle();
-        args.putInt(ARG_COLUMN_COUNT, columnCount);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        if (getArguments() != null) {
-            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
-        }
     }
 
     @Override
@@ -99,42 +75,6 @@ public class DayListFragment extends Fragment {
 
         return view;
     }
-
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnListFragmentInteractionListener) {
-            mListener = (OnListFragmentInteractionListener) context;
-        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnDayItemClickListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnListFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onListFragmentInteraction(Day item);
-    }
-
-
-
 
 
     private void setUpAdapter() {
