6815df1262ec6454a42ee0f6102df13f23d9775c
Adrian Campos <adriancampos@users.noreply.github.com>
2020-03-04 13:15:28 -0800

    Got notifications to fire
    Needed to tell the system about our notification channel

--- app/src/main/java/mud/arca/io/mud/Notifications/AlarmReceiver.java
+++ app/src/main/java/mud/arca/io/mud/Notifications/AlarmReceiver.java
@@ -1,5 +1,6 @@
 package mud.arca.io.mud.Notifications;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
@@ -7,20 +8,24 @@ import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 
 import androidx.core.app.NotificationCompat;
 
+import androidx.core.app.NotificationManagerCompat;
 import mud.arca.io.mud.Util.Util;
 import mud.arca.io.mud.MainActivity;
 import mud.arca.io.mud.R;
 
 public class AlarmReceiver extends BroadcastReceiver {
 
-    public int id = 0;
-
+    public static int id = 0;
+    public static String MOOD_REMINDER_CHANNEL_ID = "mood_reminder";
     @Override
     public void onReceive(Context context, Intent intent) {
         Util.debug("onReceive() called ******************");
+
+        createNotificationChannel(context);
 
         long when = System.currentTimeMillis();
         NotificationManager notificationManager = (NotificationManager) context
@@ -37,7 +42,7 @@ public class AlarmReceiver extends BroadcastReceiver {
         Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 
         NotificationCompat.Builder mNotifyBuilder = new NotificationCompat.Builder(
-                context)
+                context, MOOD_REMINDER_CHANNEL_ID)
                 //.setSmallIcon(R.drawable.applogo)
                 .setSmallIcon(R.drawable.ic_launcher_foreground)
                 .setContentTitle("Alarm Fired")
@@ -48,6 +53,22 @@ public class AlarmReceiver extends BroadcastReceiver {
         notificationManager.notify(id, mNotifyBuilder.build());
         id++;
 
+    }
+
+    private void createNotificationChannel(Context context) {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Daily Mood Reminders"; // TODO: Export to getResources
+            String description ="Very long channel description that supports\nmultiple\nlines\netc..."; // TODO: Export to getResources
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel(MOOD_REMINDER_CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
 }
