589a9988888dc51953c786c803a9f9c612025da4
Adrian Campos <adriancampos@users.noreply.github.com>
2020-03-04 11:19:11 -0800

    Cleaned up Variable class
    Removed deprecated vardata field

--- app/src/main/java/mud/arca/io/mud/DataStructures/Variable.java
+++ app/src/main/java/mud/arca/io/mud/DataStructures/Variable.java
@@ -7,7 +7,6 @@ public class Variable implements Serializable {
     private String name;
     private String unit;
     private VarType vartype;
-    private float vardata;
 
     public enum VarType
     {
@@ -70,20 +69,6 @@ public class Variable implements Serializable {
 
     public void setVartype(VarType vartype) {
         this.vartype = vartype;
-    }
-
-    public void setVardata(float vardata) {this.vardata = vardata;}
-
-    /**
-     * setVardata override to accept boolean value
-     * @param vardata boolean value to translate into workable data
-     */
-    public void setVardata(boolean vardata) {
-
-        // verify that boolean matches with VarType enum
-
-        // convert bool into assigned float number
-
     }
 
 
