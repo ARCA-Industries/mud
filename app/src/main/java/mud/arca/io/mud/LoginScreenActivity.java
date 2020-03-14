210cf54077764cf9e25ce6c7d7cc3bffdc328221
Adrian Campos <adriancampos@users.noreply.github.com>
2020-03-14 15:19:12 -0700

    Cleaned up LoginScreenActivity

--- app/src/main/java/mud/arca/io/mud/LoginScreenActivity.java
+++ app/src/main/java/mud/arca/io/mud/LoginScreenActivity.java
@@ -2,25 +2,15 @@ package mud.arca.io.mud;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthMethodPickerLayout;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -34,8 +24,6 @@ public class LoginScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.login_activity);
-
-        //findViewById(R.id.sign_in_button).setOnClickListener(v -> login());
 
         if (FirebaseAuth.getInstance().getCurrentUser() != null) {
             // If the user's already logged in, just continue to MainActivity
