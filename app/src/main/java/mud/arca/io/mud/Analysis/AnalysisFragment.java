965a40788a14587cfddcf2b30fd4bae3c23c60de
Andrew Steffen <3031930+asteffen@users.noreply.github.com>
2020-03-25 09:36:50 -0700

    Basic implementation of date dropdown on analysis fragment. Create class ItemToSelectDays.

--- app/src/main/java/mud/arca/io/mud/Analysis/AnalysisFragment.java
+++ app/src/main/java/mud/arca/io/mud/Analysis/AnalysisFragment.java
@@ -2,6 +2,7 @@ package mud.arca.io.mud.Analysis;
 
 import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
+import android.content.ClipData;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -18,6 +19,7 @@ import android.widget.FrameLayout;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -60,26 +62,142 @@ public class AnalysisFragment extends Fragment implements FragmentWithMenu {
         }
     }
 
+    /**
+     * DateSelector is used to initialize an EditText, so that it pops up a date picker dialog
+     * when clicked. The date field keeps track of the date the user selects in the dialog.
+     */
+    public class DateSelector {
+        public Date date;
+        public EditText et;
+        public boolean isStartDate;
+
+        public void setDate(Date dateSelected) {
+            // Set text of EditText
+            et.setText(Util.formatDateWithYear(dateSelected));
+            // Save to field
+            date = dateSelected;
+            // Save to field in AnalysisFragment
+            if (isStartDate) {
+                startDate = dateSelected;
+            } else {
+                endDate = dateSelected;
+            }
+        }
+
+        public DateSelector(View view, EditText et, boolean isStartDate) {
+            this.et = et;
+            this.isStartDate = isStartDate;
+
+            // Stop the keyboard from popping up when the EditText is clicked.
+            et.setShowSoftInputOnFocus(false);
+            // Disable blinking cursor
+            et.setCursorVisible(false);
+            // We have to set focusable to false, otherwise the user must click twice to set the date.
+            et.setFocusable(false);
+
+            et.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker view, int year, int month, int day) {
+                            Date dateSelected = new GregorianCalendar(year, month, day).getTime();
+                            setDate(dateSelected);
+
+                            Util.debug("onDateSet() called");
+                            updatePlot();
+                        }
+                    };
+
+                    Calendar cal = Calendar.getInstance();
+                    cal.setTime(date);
+                    int oldDay = cal.get(Calendar.DAY_OF_MONTH);
+                    int oldMonth = cal.get(Calendar.MONTH);
+                    int oldYear = cal.get(Calendar.YEAR);
+
+                    boolean useSpinner = sharedPrefs.getBoolean("use_spinner_datepicker", false);
+                    int themeResId = useSpinner ? R.style.MySpinnerDatePickerStyle : R.style.MyCalendarDatePickerStyle;
+
+                    // Initialize the DatePickerDialog with the old day selected.
+                    DatePickerDialog picker = new DatePickerDialog(view.getContext(),
+                            themeResId, listener, oldYear, oldMonth, oldDay);
+
+                    // Restrict the dates in DatePickerDialog.
+                    Date minDate;
+                    Date maxDate;
+                    if (isStartDate) {
+                        minDate = earliestDate;
+                        maxDate = endDate;
+                    } else {
+                        minDate = startDate;
+                        maxDate = latestDate;
+                    }
+                    picker.getDatePicker().setMinDate(minDate.getTime());
+                    picker.getDatePicker().setMaxDate(maxDate.getTime());
+
+                    picker.show();
+                    // TODO: There is no gap between buttons, fix it
+                    //Button b1 = picker.getButton(DialogInterface.BUTTON_NEGATIVE);
+                }
+            });
+
+            // Set the initial date of the DateSelector
+            if (isStartDate) {
+                setDate(startDate);
+            } else {
+                setDate(latestDate);
+            }
+        }
+    }
+
+    /**
+     * This class is used to generate dropdown items where the user can select last 7 days, last 30 days, etc.
+     */
+    public class ItemToSelectDays {
+        private int numDays;
+
+        public ItemToSelectDays(int numDays) {
+            this.numDays = numDays;
+        }
+
+        public String getText() {
+            return String.format("Plot last %d days", numDays);
+        }
+
+        public void applyToDateSelectors() {
+            endDS.setDate(latestDate);
+            startDS.setDate(Util.intToDate(latestDate, -numDays));
+            updatePlot();
+        }
+    }
+
+    public List<ItemToSelectDays> menuDropdownItems = Arrays.asList(
+            new ItemToSelectDays(7),
+            new ItemToSelectDays(30)
+    );
+
     @Override
     public void onThreeDotsClicked(View anchor) {
         PopupMenu menu = new PopupMenu(getActivity(), anchor);
-        menu.getMenu().add(Menu.NONE, 1, 0, "Share");
-        menu.getMenu().add(Menu.NONE, 2, 1, "Comment");
+
+        for (int id = 0; id < menuDropdownItems.size(); id++) {
+            ItemToSelectDays item = menuDropdownItems.get(id);
+            menu.getMenu().add(Menu.NONE, id, id, item.getText());
+        }
+
         menu.show();
 
         menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
 
-                int i = item.getItemId();
-                if (i == 1) {
-                    //handle share
-                    return true;
-                } else if (i == 2) {
-                    //handle comment
-                    return true;
-                } else {
+                int id = item.getItemId();
+                if (id >= menuDropdownItems.size()) {
+                    // This should never happen, it should always be that case that id < menuDropdownItems.size().
                     return false;
+                } else {
+                    menuDropdownItems.get(id).applyToDateSelectors();
+                    return true;
                 }
             }
         });
@@ -245,94 +363,6 @@ public class AnalysisFragment extends Fragment implements FragmentWithMenu {
         startDS = new DateSelector(getView(), startET, true);
         EditText endET = getView().findViewById(R.id.inputEndEditText);
         endDS = new DateSelector(getView(), endET, false);
-    }
-
-    /**
-     * DateSelector is used to initialize an EditText, so that it pops up a date picker dialog
-     * when clicked. The date field keeps track of the date the user selects in the dialog.
-     */
-    public class DateSelector {
-        public Date date;
-        public EditText et;
-        public boolean isStartDate;
-
-        public void setDate(Date dateSelected) {
-            // Set text of EditText
-            et.setText(Util.formatDateWithYear(dateSelected));
-            // Save to field
-            date = dateSelected;
-            // Save to field in AnalysisFragment
-            if (isStartDate) {
-                startDate = dateSelected;
-            } else {
-                endDate = dateSelected;
-            }
-        }
-
-        public DateSelector(View view, EditText et, boolean isStartDate) {
-            this.et = et;
-            this.isStartDate = isStartDate;
-
-            // Stop the keyboard from popping up when the EditText is clicked.
-            et.setShowSoftInputOnFocus(false);
-            // Disable blinking cursor
-            et.setCursorVisible(false);
-            // We have to set focusable to false, otherwise the user must click twice to set the date.
-            et.setFocusable(false);
-
-            et.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
-                        @Override
-                        public void onDateSet(DatePicker view, int year, int month, int day) {
-                            Date dateSelected = new GregorianCalendar(year, month, day).getTime();
-                            setDate(dateSelected);
-
-                            Util.debug("onDateSet() called");
-                            updatePlot();
-                        }
-                    };
-
-                    Calendar cal = Calendar.getInstance();
-                    cal.setTime(date);
-                    int oldDay = cal.get(Calendar.DAY_OF_MONTH);
-                    int oldMonth = cal.get(Calendar.MONTH);
-                    int oldYear = cal.get(Calendar.YEAR);
-
-                    boolean useSpinner = sharedPrefs.getBoolean("use_spinner_datepicker", false);
-                    int themeResId = useSpinner ? R.style.MySpinnerDatePickerStyle : R.style.MyCalendarDatePickerStyle;
-
-                    // Initialize the DatePickerDialog with the old day selected.
-                    DatePickerDialog picker = new DatePickerDialog(view.getContext(),
-                            themeResId, listener, oldYear, oldMonth, oldDay);
-
-                    // Restrict the dates in DatePickerDialog.
-                    Date minDate;
-                    Date maxDate;
-                    if (isStartDate) {
-                        minDate = earliestDate;
-                        maxDate = endDate;
-                    } else {
-                        minDate = startDate;
-                        maxDate = latestDate;
-                    }
-                    picker.getDatePicker().setMinDate(minDate.getTime());
-                    picker.getDatePicker().setMaxDate(maxDate.getTime());
-
-                    picker.show();
-                    // TODO: There is no gap between buttons, fix it
-                    //Button b1 = picker.getButton(DialogInterface.BUTTON_NEGATIVE);
-                }
-            });
-
-            // Set the initial date of the DateSelector
-            if (isStartDate) {
-                setDate(startDate);
-            } else {
-                setDate(latestDate);
-            }
-        }
     }
 
     @Override
